7b1fdecc020da797ccc7e3e57835da66f81d8567
tushararora02 <tushararora2496@gmail.com>
2024-11-11 19:18:12 -0500

    JWT POC using JJWT and spring security

---
+++ springboot-jwt-auth/src/main/java/com/demo/authapi/services/UserService.java
@@ -0,0 +1,25 @@
+package com.demo.authapi.services;
+
+import com.demo.authapi.entities.User;
+import com.demo.authapi.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
+}
---
+++ springboot-jwt-auth/src/test/java/com/demo/authapi/AuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.authapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
